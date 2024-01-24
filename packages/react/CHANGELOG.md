6a32d5155ac66b562711d8532f8c32bc60d2f1a2
Matheus Rabelo <matheus.nicks@gmail.com>
2024-01-24 00:52:23 -0300

    fix: Add ref to TextInput and adjust some styles

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @mattmax-solutions-ui/react
 
+## 3.0.1
+
+### Patch Changes
+
+- Add option to pass reference to textinput and adjust some styles
+
 ## 3.0.0
 
 ### Major Changes
--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -20,7 +20,7 @@ export const CheckboxContainer = styled(Checkbox.Root, {
     backgroundColor: '$mattmax400',
   },
 
-  '&:focus': {
+  '&:focus, &[data-state="checked"]': {
     border: '2px solid $mattmax400',
   },
 })
